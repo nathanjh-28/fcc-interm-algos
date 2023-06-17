3c18de29f5ee6390e9663b41187aed8b66f9ecbc
nathanjh-28 <nathan@nathanjharris.com>
2023-06-16 23:58:22 -0400

    spinal tap case prompt

---
+++ spinal-tap-case.js
@@ -0,0 +1,11 @@
+/*
+
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+*/
+
+function spinalCase(str) {
+    return str;
+}
+
+spinalCase('This Is Spinal Tap');
