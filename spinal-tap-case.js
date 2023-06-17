adb8af4a063910707ddec28a8f2a24542ba67a87
nathanjh-28 <nathan@nathanjharris.com>
2023-06-16 23:58:46 -0400

    spinal tap case complete

--- spinal-tap-case.js
+++ spinal-tap-case.js
@@ -5,7 +5,13 @@ Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-da
 */
 
 function spinalCase(str) {
-    return str;
+    const regex = /\s|_|(?=[A-Z])/
+    return str.split(regex).join('-').toLowerCase()
+
 }
 
-spinalCase('This Is Spinal Tap');
+console.log(spinalCase('This Is Spinal Tap'));
+console.log(spinalCase("The_Andy_Griffith_Show"));
+console.log(spinalCase("thisIsSpinalTap"));
+console.log(spinalCase("Teletubbies say Eh-oh"));
+console.log(spinalCase("AllThe-small Things"));
