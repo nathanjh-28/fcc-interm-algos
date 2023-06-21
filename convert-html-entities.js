7e5b0e6a7f573f30920091a53c7ab7199cb466e8
nathanjh-28 <nathan@nathanjharris.com>
2023-06-20 22:55:52 -0400

    convert html complete

--- convert-html-entities.js
+++ convert-html-entities.js
@@ -1,1 +1,20 @@
 /* Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.*/
+
+function convertHTML(str) {
+    const reg = /&|<|>|"|'/;
+    const htmlKey = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&apos;'
+    }
+
+
+
+    return str
+        .split('')
+        .map(item => reg.test(item) ? htmlKey[item] : item).join('')
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
