b15714b93dd6da6f50b68f5f0530622a15f46a60
nathanjh-28 <nathan@nathanjharris.com>
2023-06-22 12:43:19 -0400

    smallest common multiple brute force solution complete

--- smallest-common-multiple.js
+++ smallest-common-multiple.js
@@ -8,3 +8,34 @@ For example, if given 1 and 3, find the smallest common multiple of both 1 and 3
 
 */
 
+function smallestCommons(arr) {
+    const max = Math.max(...arr)
+    const min = Math.min(...arr)
+    //console.log(min,max)
+    const newArr = [];
+    for (let i = min; i <= max; i++) {
+        newArr.push(i)
+        //console.log(newArr)
+    }
+    const maxLength = newArr.reduce((a, b) => a * b)
+    let temp;
+    let validator = 0;
+    for (let i = 1; i < maxLength; i++) {
+        temp = i * max;
+        validator = 0;
+        for (let j = 0; j < newArr.length; j++) {
+            if (temp % newArr[j] === 0) validator++
+            if (validator === newArr.length) return temp
+        }
+    }
+
+
+
+
+    //return newArr;
+}
+
+//console.log(smallestCommons([1,5]));
+//console.log(smallestCommons([2,10]));
+console.log(smallestCommons([1, 13]));
+console.log(smallestCommons([23, 18]));
