29ff8a31f2ad95c0899b8ee78f8bb86f43143c1b
nathanjh-28 <nathan@nathanjharris.com>
2023-06-22 12:02:14 -0400

    sum all primes complete

--- sum-all-primes.js
+++ sum-all-primes.js
@@ -3,3 +3,23 @@ A prime number is a whole number greater than 1 with exactly two divisors: 1 and
 
 Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num. 
 */
+
+function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+function sumPrimes(num) {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        if (isPrime(i)) sum += i;
+    }
+    return sum;
+}
+
+console.log(sumPrimes(10));
+console.log(sumPrimes(977));
