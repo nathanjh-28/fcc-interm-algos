6751a8549198577e7a6d4204b5133e89bfc4c63d
nathanjh-28 <nathan@nathanjharris.com>
2023-06-21 22:41:48 -0400

    sum odd fib nums complete

--- sum-all-fib-nums.js
+++ sum-all-fib-nums.js
@@ -5,7 +5,15 @@ The first two numbers in the Fibonacci sequence are 0 and 1. Every additional nu
 For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5. */
 
 function sumFibs(num) {
-    return num;
+    let sum = 1;
+    let arr = [0, 1];
+    while ((arr[arr.length - 1]) + (arr[arr.length - 2]) <= num) {
+
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        if (arr[arr.length - 1] % 2 === 1) sum += arr[arr.length - 1];
+    }
+    return sum;
 }
 
-sumFibs(4);
+console.log(sumFibs(10));
+console.log(sumFibs(75025))
