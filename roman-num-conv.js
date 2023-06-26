6002d61167ecc6ddcf362da870105d9702fe6d48
nathanjh-28 <nathan@nathanjharris.com>
2023-06-25 20:15:35 -0400

    roman numeral converter obj refactored

--- roman-num-conv.js
+++ roman-num-conv.js
@@ -1,52 +1,45 @@
 function convertToRoman(num) {
+    if (num === 0 || typeof num !== 'number') return undefined;
+
+
     return num;
 }
 
 convertToRoman(36);
 
-function one(num) {
-    switch (num) {
-        case num == 0: return 'X'
-        case num == 5: return 'V'
-        case num == 1: return 'I'
-        case num == 2: return 'II'
-        case num == 3: return 'III'
-        case num == 4: return 'IV'
-        case num == 6: return 'VI'
-        case num == 7: return 'VII'
-        case num == 8: return 'VIII'
-        case num == 9: return 'IX'
-    }
+let romanNumeralObj = {
+    "1": 'I',
+    "2": 'II',
+    "3": 'III',
+    "4": 'IV',
+    "5": 'V',
+    "6": 'VI',
+    "7": 'VII',
+    "8": 'VIII',
+    "9": 'IX',
+    "10": 'X',
+    "20": 'XX',
+    "30": 'XXX',
+    "40": 'XL',
+    "50": 'L',
+    "60": 'LX',
+    "70": 'LXX',
+    "80": 'LXXX',
+    "90": 'XC',
+    "100": 'C',
+    "200": 'CC',
+    "300": 'CCC',
+    "400": 'CD',
+    "500": 'D',
+    "600": 'DC',
+    "700": 'DCC',
+    "800": 'DCCC',
+    "900": 'CM',
+    "1000": 'M',
+    "2000": 'MM',
+    "3000": 'MMM',
 }
 
-function two(num) {
-    switch (num) {
-        case num == 10: return 'X'
-        case num == 50: return 'L'
-        case num == 100: return 'C'
-        case num == 20: return 'XX'
-        case num == 30: return 'XXX'
-        case num == 40: return 'XL'
-        case num == 60: return 'LX'
-        case num == 70: return 'LXX'
-        case num == 80: return 'LXXX'
-        case num == 90: return 'XC'
-    }
-}
 
-function three(num) {
-    switch (num) {
-        case num == 100: return 'C'
-        case num == 200: return 'CC'
-        case num == 300: return 'CCC'
-        case num == 400: return 'CD'
-        case num == 500: return 'D'
-        case num == 600: return 'DC'
-        case num == 700: return 'DCC'
-        case num == 800: return 'DCCC'
-        case num == 900: return 'CM'
-        case num == 1000: return ''
 
-    }
-}
 
