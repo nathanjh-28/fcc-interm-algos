7c8522e7c4a9e128a97b353784d66b64e79f0d95
nathanjh-28 <nathan@nathanjharris.com>
2023-06-30 13:18:00 -0400

    inventory update partial solution

--- interview-prep/inventory-update.js
+++ interview-prep/inventory-update.js
@@ -3,3 +3,19 @@ Inventory Update
 Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
 */
 
+function updateInventory(arr1, arr2) {
+    let found = false;
+    for (let i = 0; i < arr2.length; i++) {
+        found = false;
+        for (let j = 0; j < arr1.length; j++) {
+            if (arr2[i][1] === arr1[j][1]) {
+                arr1[j][0] += arr2[i][0];
+                found = true;
+            }
+
+        }
+        if (!found) arr1.push(arr2[i])
+    }
+
+    return arr1;
+}
